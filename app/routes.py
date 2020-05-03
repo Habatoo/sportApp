03c5d237ab8c04ce924f4d992e85c38ed1a67a89
Habatoo <habatoo@gmail.com>
2020-05-03 21:22:41 +0700

    add user url

--- app/routes.py
+++ app/routes.py
@@ -18,6 +18,7 @@ def before_request():
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    user = User.query.filter_by(username=current_user.username).first_or_404()
     form = PostForm()
     if form.validate_on_submit():
         post = Post(body=form.post.data, author=current_user)
@@ -34,8 +35,29 @@ def index():
         if posts.has_prev else None
     return render_template('index.html', title='Home', form=form,
                            posts=posts.items, next_url=next_url,
-                           prev_url=prev_url)
+                           prev_url=prev_url, user=user)
 
+@app.route('/posts', methods=['GET', 'POST'])
+@login_required
+def posts():
+    user = User.query.filter_by(username=current_user.username).first_or_404()
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form,
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url, user=user)
 
 @app.route('/explore')
 @login_required
